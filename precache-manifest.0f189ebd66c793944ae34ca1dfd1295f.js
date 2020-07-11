195be44ccaec76da4beaf9fc83c5ea0db97f9c08
abid7 <abidshahriar7@gmail.com>
2020-07-12 00:50:48 +0600

    Updates

---
+++ precache-manifest.0f189ebd66c793944ae34ca1dfd1295f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62907ad9df2bd83b2668b7f2ae84a5dd",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "998e9d5ebfe3a944d822",
+    "url": "/monster-rolodex/static/css/main.d887b907.chunk.css"
+  },
+  {
+    "revision": "b81419f85758868b9ced",
+    "url": "/monster-rolodex/static/js/2.62d78548.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.62d78548.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "998e9d5ebfe3a944d822",
+    "url": "/monster-rolodex/static/js/main.4b4c2e20.chunk.js"
+  },
+  {
+    "revision": "85cb2c00df0e1ad7d48d",
+    "url": "/monster-rolodex/static/js/runtime-main.3e508d1c.js"
+  }
+]);
